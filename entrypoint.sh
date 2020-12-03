ee8dea24a7abc1f904798a7e5d217e0362420063
Erno Rigo <erno@rigo.info>
2020-12-03 11:33:04 +0100

    remove stale lockfile on exit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 TEAMS_PATH="/usr/share/teams/teams"
-TEAMS_LOGS="$HOME/.config/Microsoft/Microsoft Teams/logs"
+TEAMS_CONFDIR="$HOME/.config/Microsoft/Microsoft Teams"
+TEAMS_LOGS="$TEAMS_CONFDIR/logs"
+trap "rm -f '$TEAMS_CONFDIR/SingletonLock'" exit
 mkdir -p "$TEAMS_LOGS"
 "$TEAMS_PATH" "$@"
 cat ~/.config/Microsoft/Microsoft\ Teams/logs.txt
